ab4d5d3eac60998250729e1e165ea5e61833b853
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2023-02-15 22:21:20 +0000

    Started the resizing of the board

--- script.js
+++ script.js
@@ -1,32 +1,94 @@
 console.log("Hello")
 
-let size = 16
+let size = 20
 
-
+let color = "white"
+//Buttons from the page
 const create = document.getElementById("create");
 const reset = document.getElementById("reset");
 const container = document.getElementById("board");
-container.style.backgroundColor = "red";
-container.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr`
+const black = document.getElementById("black");
+const red = document.getElementById("red");
+const erase = document.getElementById("erase");
+const board = document.getElementById("board");
+const ten = document.getElementById("10x10");
+
+
+container.style.backgroundColor = "white";
+// container.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr`
 
 
 
-let makeSomething = function(){
+let makeGrid = function(){
+    container.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr`
     for (let i = 0; i < size * size; i++){
-        let cell = document.createElement("div");
-        document.getElementById("board").appendChild(cell);
+        const cell = document.createElement("div");
+        cell.classList.add("boxes")
+        container.appendChild(cell).classList.add("boxes");
         cell.addEventListener("mouseover", function(){
-            cell.style.backgroundColor = "white";
+            cell.style.backgroundColor = `${color}`;
         })
+        }
     }
+
+// let resetBoard = function(){
+//     board.innerHTML = "";
+// }
+
+let blank = function(){
+    board.innerHTML = ""
 }
 
-create.addEventListener("click", makeSomething);
+let resetBoard = function(){
+    ten.addEventListener("click", function(){
+        blank();
+        size = 10;
+        makeGrid();
+    })
+
+}
 
 
-create.addEventListener("click", function(){
-    create.disabled = true;
-})
+let changeColor = function(){
+    black.addEventListener("click", function(){
+        color = "black";
+    })
+    red.addEventListener("click", function(){
+        color = "red";
+    })
+    erase.addEventListener("click", function(){
+        color = "white";
+    })
+}
+
+
+
+
+
+let play = function(){
+    create.addEventListener("click", makeGrid);
+    reset.addEventListener("click", resetBoard);
+    changeColor();
+
+}
+
+
+
+play();
+
+
+
+
+
+
+
+// create.addEventListener("click", function(){
+//     create.disabled = true;
+// })
+
+// reset.addEventListener("click", function(){
+//     cell.style.backgroundColor = "white";
+// })
 
 
 
