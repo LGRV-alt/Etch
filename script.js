cf14fa4c138a87cc251e3def7b5da01005e32002
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2023-02-18 17:10:23 +0000

    Random color added. Working on removing interval

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 console.log("Hello")
 
 let size = 20
-
 let color = "white"
+
+let changing = "noChange"
+
 //Buttons from the page
-const create = document.getElementById("create");
 const reset = document.getElementById("reset");
 const container = document.getElementById("board");
 const black = document.getElementById("black");
-const red = document.getElementById("red");
+const random = document.getElementById("random");
 const erase = document.getElementById("erase");
-const board = document.getElementById("board");
 const ten = document.getElementById("10x10");
+const twenty = document.getElementById("20x20");
+const thirty = document.getElementById("30x30");
 
 
 container.style.backgroundColor = "white";
-// container.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr`
+
 
 
 
@@ -31,64 +33,125 @@ let makeGrid = function(){
         }
     }
 
-// let resetBoard = function(){
-//     board.innerHTML = "";
-// }
 
+
+// Removes the DOM items 
 let blank = function(){
     board.innerHTML = ""
 }
 
-let resetBoard = function(){
-    ten.addEventListener("click", function(){
-        blank();
-        size = 10;
-        makeGrid();
-    })
-
+// Resets the player board 
+let = resetBoard = function(){
+    blank();
+    makeGrid();
 }
 
+// Makes the board 10X10
+let makeTen = function(){
+    blank();
+    size = 10;
+    makeGrid();
+}
 
+// Makes the board 20X20
+let makeTwenty = function(){
+    blank();
+    size = 20;
+    makeGrid();
+}
+
+// Makes the board 30X30 
+let makeThirty = function(){
+    blank();
+    size = 30;
+    makeGrid();
+}
+
+// Color changes with linked to buttons
 let changeColor = function(){
     black.addEventListener("click", function(){
+        changing = "noChange";
         color = "black";
+        colorChanging();
+        console.log(changing)
+        console.log(color);
     })
-    red.addEventListener("click", function(){
-        color = "red";
+
+    random.addEventListener("click", function(){
+        changing = "change";
+        colorChanging();
+        console.log(changing)
+        console.log(color);
     })
+
     erase.addEventListener("click", function(){
         color = "white";
+        console.log(changing)
     })
+
 }
 
 
 
+// Creates a string of 6 with random numbers and letters
+function generateColor(){
+    const hexArray = [0,1,2,3,4,5,6,7,8,9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    let color = "";
+    for (let i=0; i<6; i++){
+        color += hexArray[Math.floor(Math.random()*16)];
+    }
+    return `#${color}`
+}
+
+// Sets the generateColor() function change every 10ms
+// function randomColor(){
+//     if(changing !== "noChange"){
+//     setInterval(
+//         function() {
+//             color = generateColor();
+//         }, 1)}else{
 
 
+//         }
+// }
+
+
+function randomColor(){
+    color = generateColor();
+}
+
+function colorChanging(){
+    if(changing ==="change"){
+        setInterval(randomColor, 1);
+    } else{
+        clearInterval(randomColor);
+    }
+}
+
+
+
+// Game function
 let play = function(){
-    create.addEventListener("click", makeGrid);
+    makeGrid();
+    
     reset.addEventListener("click", resetBoard);
+    ten.addEventListener("click", makeTen);
+    twenty.addEventListener("click", makeTwenty);
+    thirty.addEventListener("click", makeThirty);
+    // random.addEventListener("click", colorChanging);
     changeColor();
+    
+
 
 }
 
 
-
+// Calls to play the game
 play();
 
 
 
 
-
-
-
-// create.addEventListener("click", function(){
-//     create.disabled = true;
-// })
-
-// reset.addEventListener("click", function(){
-//     cell.style.backgroundColor = "white";
-// })
 
 
 
